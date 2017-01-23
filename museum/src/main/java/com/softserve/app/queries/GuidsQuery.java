b0e908493cd97571ff48ec65a5e2c48700558b28
GitToasterhub <abulfazahmadov@gmail.com>
2017-01-23 11:27:08 +0200

    worke

--- museum/src/main/java/com/softserve/app/queries/GuidsQuery.java
+++ museum/src/main/java/com/softserve/app/queries/GuidsQuery.java
@@ -1,5 +1,33 @@
 package com.softserve.app.queries;
 
-public class GuidsQuery {
+import java.util.List;
 
+import com.softserve.app.entity.Exhibit;
+
+public class GuidsQuery implements Query {
+
+	@Override
+	public List executeQuery(SessionFactory factory,String ...args) { //args not used in this method as we take all Exhibits without any constraints
+		
+		Session session=factory.openSession();
+		List workers=null;
+		String sql = "select * from workers WHERE post='Guide';";
+	    Transaction tx = null;
+	    try{
+	         tx = session.beginTransaction();
+	         SQLQuery query = session.createSQLQuery(sql);
+	         query.addEntity(Exhibit.class);
+	         workers = query.list(); // here we get the list of all exhibits in base
+	       }
+	         tx.commit();
+	     }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	     }finally {
+	         session.close(); 
+	     }
+		return exhibits;
+	}
+    
+	
 }
