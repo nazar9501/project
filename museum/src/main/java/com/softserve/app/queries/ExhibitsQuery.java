3a7b476dbe9bd0fe68d658252ecd35a4cbb3928e
GitToasterhub <abulfazahmadov@gmail.com>
2017-01-23 10:36:19 +0200

    exquery

--- museum/src/main/java/com/softserve/app/queries/ExhibitsQuery.java
+++ museum/src/main/java/com/softserve/app/queries/ExhibitsQuery.java
@@ -2,13 +2,30 @@ package com.softserve.app.queries;
 
 import java.util.List;
 
+import com.softserve.app.entity.Exhibit;
+
 public class ExhibitsQuery implements Query {
 
 	@Override
-	public List executeQuery(SessionFactory factory) {
-		List exhibits;
-		Session session=
+	public List executeQuery(SessionFactory factory,String ...args) { //args not used in this method as we take all Exhibits without any constraints
+		Session session=factory.openSession();
+		String sql = "select * from exhibit;";
+	    Transaction tx = null;
+	    try{
+	         tx = session.beginTransaction();
+	         SQLQuery query = session.createSQLQuery(sql);
+	         query.addEntity(Exhibit.class);
+	         exhibits = query.list(); // here we get the list of all exhibits in base
+	       }
+	         tx.commit();
+	     }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	     }finally {
+	         session.close(); 
+	     }
 		return exhibits;
 	}
+    
 	
 }
